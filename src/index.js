0ca85beb14cee4c042d072287e2c8147a95965de
stevenrescigno <steven@symphonyagency.com>
2019-10-12 22:00:14 -0400

    Add disable setting to input on success to improve ui experience

--- src/index.js
+++ src/index.js
@@ -46,6 +46,7 @@ class Mailchimp extends React.Component {
           <input
             {...input}
             key={input.name}
+            disabled={status === "success"}
             onChange={({ target }) => this.setState({ [input.name]: target.value })}
             defaultValue={this.state[input.name]}
           />
